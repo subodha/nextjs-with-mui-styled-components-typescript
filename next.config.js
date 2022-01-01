6db0548a1d34691850d582f5c4b6288ef848a9bd
Subodha Pathiraja <subodha@atomix.com.au>
2022-01-02 02:35:58 +0530

    removing webpack config from the next.config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const withTM = require('next-transpile-modules')(['@mui/material', '@mui/system'
 module.exports = withTM({
   reactStrictMode: true,
   styledComponents: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-    };
-    return config;
-  },
 });
