97e19cd555cb5d286d83c1c3f13f95b83a18c2ba
Subodha Pathiraja <pmsubodha@gmail.com>
2022-01-11 17:33:12 +0530

    next configuration update. adding swc settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-const withTM = require('next-transpile-modules')(['@mui/material', '@mui/system']); // pass the modules you would like to see transpiled
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system"
+]); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
   reactStrictMode: true,
-  styledComponents: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc"
+    };
+    return config;
+  }
 });
